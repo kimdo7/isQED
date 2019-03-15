95aae2e8cba4e53efbd72546e95b0c6a0071d1b3
Kim Do <kim.h.do.seven@gmail.com>
2019-03-15 14:19:59 -0700

    fix about routing

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './component/body/home/home.component';
+import { AboutComponent } from './component/body/about/about.component';
 
 const routes: Routes = [
     {path:"", component: HomeComponent},
-    {path:"about", component: HomeComponent}
+    {path:"about", component: AboutComponent}
 ];
 
 @NgModule({
