93194e952066bb72a571b376424d737c074e64a0
Kim Do <kim.h.do.seven@gmail.com>
2019-03-15 12:52:56 -0700

    add header & footer

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule}  from '@angular/platform-browser/animations';
 import { MaterialModule } from './materials';
+import { HomeComponent } from './component/home/home.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HeaderComponent } from './component/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
