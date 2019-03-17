e446b36e3a0e836ca6fcf1987ed980b7d8bd0db0
Kim Do <kim.h.do.seven@gmail.com>
2019-03-17 09:20:53 -0700

    fix nav bar and add jquery

--- public/src/app/component/header/header.component.ts
+++ public/src/app/component/header/header.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        $(document).ready(function () {
+            $(window).scroll(function () {
+                var headerTop = $('#header-logo').height();
+                // console.log(aTop)
+                if ($(this).scrollTop() >= headerTop + 50) {
+                    $("#navbar").addClass("fixed-top")
+                }else{
+                    $("#navbar").removeClass("fixed-top")
+                }
+            })
+        });
+    }
 
 }
+
+// fixed-top
