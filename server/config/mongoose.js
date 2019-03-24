c8e333e88d426181a769a8d92968bcf1d64e8127
Sylvia <sylviah.yu@gmail.com>
2019-03-24 10:53:50 -0700

    added basic crud for all, and added user controller, tested on postman

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,30 +1,20 @@
 var path = require('path');
 var fs   = require("fs")
 
-/**
- * *create* a variable that points to the models folder
- */
-var models_path = path.join(__dirname, './../model');
 
 /**
  * *read all of the files* in the models_path and require (run) each of the javascript files
  */
-// 
 
-var geese = [];
-readFiles(models_path, geese)
+// kims from squre_puzzle
+// create a variable that points to the models folder
+var models_path = path.join(__dirname, './../model');
+// read all of the files in the models_path and require (run) each of the javascript files
+fs.readdirSync(models_path).forEach(function (file) {
+    if (file.indexOf('.js') >= 0) {
+        // require the file (this runs the model file which registers the schema)
+        console.log("loading " + file);
+        require(models_path + '/' + file);
+    }
+})
 
-function readFiles(curr_path, geese){
-    console.log("SY reading " + curr_path);
-    fs.readdirSync(curr_path).forEach(function (file) {
-        if (file.indexOf('.js') >= 0) {
-            console.log("SY requiring " + file);
-            var goose = require(curr_path + '/' + file);
-            geese.push(goose);
-        } else {
-            readFiles(curr_path + '/' + file, geese);
-        }
-    })
-}
-
-module.exports = geese;
