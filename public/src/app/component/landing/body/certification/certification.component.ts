fbfe84c920f1aa384ddf55d88f5f58522c84db89
Daniel Lam <danlam@ucdavis.edu>
2019-04-01 00:14:06 -0700

    Completed Certification

--- public/src/app/component/landing/body/certification/certification.component.ts
+++ public/src/app/component/landing/body/certification/certification.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-certification',
@@ -10,6 +11,9 @@ export class CertificationComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+
+
   }
 
 }
