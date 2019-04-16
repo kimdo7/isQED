2a2a01ba02679605b4b5af0101a72c7e192c7846
Kim Do <kim.h.do.seven@gmail.com>
2019-04-16 12:47:40 -0700

    add mbdpro-admin:

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './component/landing/landing.component';
 import { LandingHomeComponent } from './component/landing/landing-body/landing-home/landing-home.component';
 import { LandingEventComponent } from './component/landing/landing-body/landing-event/landing-event.component';
+import { ContactUsComponent } from './component/landing/landing-body/landing-about/contact-us/contact-us.component';
 
 const routes: Routes = [
     {
         path: "", component: LandingComponent, children: [
             { path: "", component: LandingHomeComponent },
-            { path: "events", component:  LandingEventComponent}
+            { path: "events", component:  LandingEventComponent},
+            { path: "contact-us", component:  ContactUsComponent}
         ]
     }
 ];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -30,7 +30,10 @@ import { MDBBootstrapModulesPro, MDBModalService, MDBSpinningPreloader } from 'n
 import { HttpClientModule } from '@angular/common/http';
 import { UserService } from './service/user/user.service';
 
-// import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
+
+import { ContactUsComponent } from './component/landing/landing-body/landing-about/contact-us/contact-us.component';
+
+
 
 
 @NgModule({
@@ -62,6 +65,7 @@ import { UserService } from './service/user/user.service';
         RegisterModalComponent,
         ForgotPasswordModalComponent,
         ContactUsModalComponent,
+        ContactUsComponent,
     ],
     imports: [
         BrowserModule,
