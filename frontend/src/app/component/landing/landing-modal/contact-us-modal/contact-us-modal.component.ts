cc65200b5cf3252c545ead995d764a031c18d76f
Kim Do <kim.h.do.seven@gmail.com>
2019-04-17 23:29:37 -0700

    update landing page for mobile tablet and desktop

--- frontend/src/app/component/landing/landing-modal/contact-us-modal/contact-us-modal.component.ts
+++ frontend/src/app/component/landing/landing-modal/contact-us-modal/contact-us-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MDBModalRef } from 'ng-uikit-pro-standard';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-contact-us-modal',
@@ -8,9 +9,38 @@ import { MDBModalRef } from 'ng-uikit-pro-standard';
 })
 export class ContactUsModalComponent implements OnInit {
 
-    constructor(public modalRef: MDBModalRef) { }
+    // form_group : FormGroup
+
+    constructor(
+        public modalRef: MDBModalRef,
+        // private formBuilder: FormBuilder,
+        ) { }
 
     ngOnInit() {
+        // this.form_group = this.formBuilder.group({
+        //     first_name: ['',
+        //         [
+        //             Validators.required,
+        //             Validators.minLength(2),
+        //             Validators.pattern('^[A-Za-z ]+$')
+        //         ]
+        //     ],
+        //     last_name: ['',
+        //         [
+        //             Validators.required,
+        //             Validators.minLength(2),
+        //             Validators.pattern('^[A-Za-z ]+$')
+        //         ]
+        //     ],
+
+        //     email: ['', [Validators.required, Validators.email]],
+        //     password: ['',
+        //         [
+        //             Validators.required,
+        //             Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9].{7,}')
+        //         ]
+        //     ],
+        // })
     }
 
     onSubmit(){
