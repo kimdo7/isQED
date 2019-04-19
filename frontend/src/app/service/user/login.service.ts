004f6b6afdb528cd9195bb7ca49460d421f80257
Kim Do <kim.h.do.seven@gmail.com>
2019-04-19 00:29:26 -0700

    remove service

--- frontend/src/app/service/user/login.service.ts
+++ frontend/src/app/service/user/login.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class LoginService {
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  /* @param data is email and password
-  */
- login(data){
-     return this.http.post("/api/login", data)
- }
+    /* @param data is email and password
+    */
+    login(data) {
+        return this.http.post("/api/login", data)
+    }
 }
